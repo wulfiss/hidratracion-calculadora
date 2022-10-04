9930947d1460c66e5add0953d155ca209b08bc7a
wulfiss <96155980+wulfiss@users.noreply.github.com>
2022-10-04 08:52:16 -0300

    change way of storage of data in object

--- main.js
+++ main.js
@@ -1,42 +1,41 @@
 (function hidrata(){
-    let $button = document.querySelector('.btn');
 
-    let divResult = document.querySelector('#divResult');
-    let template = document.querySelector('#templateDiv').content;
+    function Chicken(initial, final){
+        this.initial = initial
+        this.final = final
+        this.diff = (this.final - this.initial).toFixed(2)
+        this.hidrata = ((this.diff * 100) / this.initial).toFixed(2)
+    }
 
-    $button.addEventListener('click', (e) =>{
-        if(e.target.id == '$one'){
-            divResult.appendChild(template);
-            calcular();
-        }
-    })
+    function addChicken(initial, final, arr){
+        
+        let newChicken = new Chicken(initial, final)
+        arr.push(newChicken);
+    }
 
     function input(){
         let initialW = document.querySelector('#initialW').value;
         let finalW = document.querySelector('#finalW').value;
-        return{
-            initialW, finalW
+        
+        return{initialW, finalW}
+    }
+
+    let chickenOne =  new Chicken(2.5, 2.7);
+
+    let arrChicken = [chickenOne];
+
+    let $button = document.querySelector('.btn');
+
+
+    let divResult = document.querySelector('#divResult');
+    let template = document.querySelector('#templateDiv').content;
+
+
+    $button.addEventListener('click', (e) =>{
+        if(e.target.id == '$one'){
+            addChicken(input()['initialW'], input()['finalW'], arrChicken);
+            console.log(arrChicken);
         }
-    }
-
-    function calcular(){
-        let diff = input().finalW - input().initialW;
-        let perc = (diff * 100) / input().initialW;
-        
-        console.log({diff, perc})
-        render(diff, perc);
-    }
-    
-    function render(diffe, perce){
-        let diffWeight = document.querySelector('#diffWeight');
-        let percentage = document.querySelector('#percentaje');
-        
-        diffWeight.textContent = diffe.toFixed(2) + 'gr.';
-        percentage.textContent = perce.toFixed(2) + '%';
-    }
-
-    return{
-        calcular: calcular
-    }
+    })
 
 })();
